c93528336181afd2cc8eafe22c105a86fd769527
eniserkaya <aeniserkaya@gmail.com>
2017-06-17 12:03:03 +0300

    Menu Eklendi

--- GYNotDefteri/app/src/main/java/com/eniserkaya/gynotdefteri/NotAlmaEkrani.java
+++ GYNotDefteri/app/src/main/java/com/eniserkaya/gynotdefteri/NotAlmaEkrani.java
@@ -3,6 +3,9 @@ package com.eniserkaya.gynotdefteri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NotAlmaEkrani extends AppCompatActivity {
     private Toolbar toolbar;
@@ -14,5 +17,26 @@ public class NotAlmaEkrani extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.toolbar_notalma);
         toolbar.setTitle("deneme");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_iki,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.sms_id:
+                Toast.makeText(this, "Sms atılacak", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mail_id:
+                Toast.makeText(this, "Mail atılacak", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
